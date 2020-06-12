4d98ac96902fbe972c9e42cd8c1d44230baac4c8
Lance W <50839441+disposableraft@users.noreply.github.com>
2020-06-12 16:32:32 -0700

    Show error when images fail to load

--- src/components/expandable-image.js
+++ src/components/expandable-image.js
@@ -1,33 +1,73 @@
 import React from "react"
 import Img from "gatsby-image"
+import { rhythm } from "../utils/typography"
 
 class ExpandableImage extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       expanded: false,
+      error: false,
     }
   }
 
   expand() {
     this.setState(state => {
-      return {
-        expanded: true,
-      }
+      state.expanded = true
+      return state
+    })
+  }
+
+  handleError(error) {
+    this.setState(state => {
+      state.error = true
+      state.expanded = false
+      return state
     })
   }
 
   render() {
+    let content
+    if (!this.state.error) {
+      content = this.state.expanded ? (
+        <Img
+          fluid={this.props.fullsize}
+          onError={error => {
+            this.handleError(error)
+          }}
+          backgroundColor={`black`}
+        />
+      ) : (
+        <Img
+          style={{ cursor: "pointer" }}
+          fluid={this.props.banner}
+          backgroundColor={`black`}
+        />
+      )
+    } else {
+      content = (
+        <div className="ExpandableImageError" style={{ position: "relative" }}>
+          <Img fluid={this.props.banner} backgroundColor={`black`}></Img>
+          <div
+            style={{
+              position: "absolute",
+              top: "40%",
+              left: "50%",
+              color: "ghostwhite",
+              textShadow: "0.05em 0.05em 0.25em black",
+              fontSize: rhythm(1),
+              textAlign: "center",
+              verticalAlign: "middle",
+            }}
+          >
+            Error :/
+          </div>
+        </div>
+      )
+    }
     return (
-      <div
-        role="presentation"
-        onClick={() => this.expand()}
-      >
-        {this.state.expanded ? (
-          <Img fluid={this.props.fullsize} />
-        ) : (
-          <Img style={{cursor:'pointer'}} fluid={this.props.banner} />
-        )}
+      <div role="presentation" onClick={() => this.expand()}>
+        {content}
       </div>
     )
   }
