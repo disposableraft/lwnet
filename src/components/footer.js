dfc819bba7dbd9930035e5f09f7bfa1bc77bef7c
Lance W <50839441+disposableraft@users.noreply.github.com>
2020-06-21 09:58:00 -0700

    Footer component and space below footer

---
+++ src/components/footer.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Link } from "gatsby"
+import { Box, Text } from "@chakra-ui/core"
+
+const Footer = () => {
+  return (
+    <Box my="8">
+      <Text fontSize="xs" textAlign="center">
+        <p>© {new Date().getFullYear()} Lance Wakeling</p>
+        <p>
+          <a href="https://instagram.com/disposableraft">Instagram</a> —{" "}
+          <a href="https://github.com/disposableraft">Github</a>
+        </p>
+      </Text>
+    </Box>
+  )
+}
+
+export default Footer
