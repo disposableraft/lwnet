9efd7d76fb63212adf0290cf3e0da00e544d2ac1
Lance W <50839441+disposableraft@users.noreply.github.com>
2020-06-20 19:08:02 -0700

    Chakrafy private circulation page

--- src/pages/private-circulation.js
+++ src/pages/private-circulation.js
@@ -1,23 +1,33 @@
 import React from "react"
-import { graphql, Link } from "gatsby"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-
+import { Text, Heading, Stack, Box, Link, Icon } from "@chakra-ui/core"
 // NOTE: So, this approach taught me about sourceInstanceName, but it's not that useful as is. That's because I'd like each issue to have: a title, a link and a description. Since there are only a handful of issues, and there won't be more in the future, it's more efficient to just use static assets.
 
 export default ({ data }) => {
   const issues = data.allFile.nodes.map(issue => {
     return (
-      <li>
-        <a href={issue.publicURL}>{issue.name}</a>
-      </li>
+      <Box
+        p={5}
+        shadow="md"
+        borderWidth="1px"
+        textAlign="center"
+        border="1px solid"
+        borderColor="gray.200"
+      >
+        <Link href={issue.publicURL} isExternal={true}>
+          {issue.name}
+          <Icon name="external-link" mx="2" color="red.500" />
+        </Link>
+      </Box>
     )
   })
   return (
     <Layout>
       <SEO title="Private Circulation" />
-      <h1>Private Circulation</h1>
-      <p>
+      <Heading>Private Circulation</Heading>
+      <Text>
         "The premier issue ..." begins page one, "has been emailed to you — that
         much is evident."
         <em>Private Circulation</em>, a monthly PDF journal published from
@@ -25,15 +35,15 @@ export default ({ data }) => {
         sincere desire to exhibit and publish. Issues ran the gamut from
         inspired, to interesting, to irrational. We appologize in advance for
         the many broken links, but this is the world we live in.
-      </p>
-      <p>
+      </Text>
+      <Text>
         <em>
           Sincerely,
           <br />
           The Editors
         </em>
-      </p>
-      <ul>{issues}</ul>
+      </Text>
+      <Stack mt="5">{issues}</Stack>
     </Layout>
   )
 }
